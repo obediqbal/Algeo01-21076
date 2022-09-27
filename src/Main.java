cbff7dea23a7359c06201fb1996d953daa84a230
haidar <89732397+haidarhamda@users.noreply.github.com>
2022-09-27 18:26:17 +0700

    reduksibaris det

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,17 @@ import Matrix.*;
 class Main{
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        //int menu = Utils.utils.getMenu();
+        //Utils.utils.getMenu();
         int n=obj.nextInt();
         int ma=obj.nextInt();
         double[][] m = new double[n][ma];
         utils.readMatrix(m,n,ma);
         utils.printMatrix(m);
-        System.out.println(determinant.ekspansiKofaktor(m));
-        Matrix.spl.cramer(m);
+        System.out.println("determinan");
+        System.out.println(determinant.reduksiBaris(m));
+        //Matrix.spl.cramer(m);
+        //Matrix.OBE.triangleup(m);
+        utils.printMatrix(m);
         obj.close();
     }
 }
--- src/Matrix/determinant.java
+++ src/Matrix/determinant.java
@@ -1,5 +1,7 @@
 package Matrix;
 
+import Utils.utils;
+
 public class determinant {
 	public static double ekspansiKofaktor(double[][] m) {
 		double det=0;
@@ -42,16 +44,38 @@ public class determinant {
 	
 	public static double reduksiBaris(double[][] m) {
 		double[][] newm = new double[m.length][m.length];
-		newm = m;
-		double det=0;
-		for(int i=0;i<m.length;i++) {
-			for(int j=0; j<m.length;j++) {
-				if (i==j) {
-					
+		spl.copyMatrix(m, newm);
+		int swapcount=0;
+		double det=1;
+		int firstidx=0;
+		for(int i=0;i<m[0].length;i++) {
+			if(newm[i][i]==0){
+				int swapidx=i;
+				while(swapidx<newm.length ){
+					if(newm[swapidx][i]!=0){
+						swap(newm,i,swapidx);
+						swapcount++;
+						break;
+					}
+					swapidx++;
 				}
 			}
+			double bawah=newm[firstidx][i];
+			for(int j=firstidx+1; j<m[0].length;j++) {
+				double atas =newm[j][i];
+				for (int k=i;k<newm[0].length;k++){
+					newm[j][k]-=(newm[firstidx][k]*atas)/bawah;
+				}
+			}
+			firstidx++;
+		}
+		utils.printMatrix(newm);
+		det=det*Math.pow(-1, swapcount);
+		for(int i=0;i<m.length;i++){
+			det=det*newm[i][i];
 		}
 		return det;
+		
 	}
 	public static void swap(double[][] m,int i1,int i2) {
 		double[] temp = m[i1];
