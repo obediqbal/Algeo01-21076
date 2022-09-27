1c8cba5936e3d85838f3fc7c6c1d595f14a0d5a5
haidar <89732397+haidarhamda@users.noreply.github.com>
2022-09-27 21:39:58 +0700

    Update interpolasiPolinom.java

--- src/interpolasi_regresi/interpolasiPolinom.java
+++ src/interpolasi_regresi/interpolasiPolinom.java
@@ -2,10 +2,9 @@ package interpolasi_regresi;
 import Matrix.*;
 
 public class interpolasiPolinom {
-	public static void interpolasipol(double[][] m) {
+	public static void interpolasipol(double[][] m,double x) {
 		double[] xs= new double[m.length];
 		spl.cramer(m, xs);
-		double x=9.2;
 		double result=0;
 		for(int i=0;i<xs.length;i++){
 			result=result+Math.pow(x, i)*xs[i];
