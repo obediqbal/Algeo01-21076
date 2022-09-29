61be28f4eb2a339258180838f7168c5c6e2000cd
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-09-29 16:48:08 +0700

    adapted OBE to reduced

--- src/Matrix/OBE.java
+++ src/Matrix/OBE.java
@@ -62,38 +62,36 @@ public class OBE {
         }
         return -1;
     }
-    private static boolean isColumnZero(double[][] m, int col, int a, int b){
+    public static boolean isColumnZero(double[][] m, int col, int a, int b){
         return findNonZero(m, col, a, b)==-1;
     }
-    private static boolean isRowZero(double[][] m, int row, int a, int b){
+    public static boolean isRowZero(double[][] m, int row, int a, int b){
         for(int j = a; j<b; j++){
             if(m[row][j]!=0) return false;
         }return true;
     }
-    public static void toEchelon(double[][] m){
+    public static void toEchelon(double[][] m, boolean reduced){
         // I.S. m matriks augmented sembarang
         // F.S. m matriks eselon augmented 
         double[][] nm = new double[utils.max(m.length, m[0].length)][m[0].length];
-        utils.fillZero(nm);
         utils.forceCopyMatrix(m, nm);
-
 
         int toprow = 0;
         int idx;
 
         for(int j = 0; j<m[0].length; j++){
             if(isColumnZero(m, j, toprow, m.length)) continue;
-            for(int i = m.length-1; i>toprow; i--){
-                if(m[toprow][j] == 0){ // prevent base OBE row to be 0
-                    idx = findNonZero(m, j, toprow, m.length);
-                    changerows(m, toprow, idx);
-                    System.out.println("switched");
-                    if(idx==-1) System.out.println("not found");
-                }
-                if(m[toprow][j] != 1){
-                    multdivrows(m, false, toprow, m[toprow][j]);
-                }
-                if(m[i][j] != 0){
+            if(m[toprow][j] == 0){ // prevent base OBE row to be 0
+                idx = findNonZero(m, j, toprow, m.length);
+                changerows(m, toprow, idx);
+                // System.out.println("switched");
+                // if(idx==-1) System.out.println("not found");
+            }
+            if(m[toprow][j] != 1){ // make sure base is 1
+                multdivrows(m, false, toprow, m[toprow][j]);
+            }
+            for(int i = m.length-1; i>(reduced ? -1 : toprow); i--){ // eliminate into 0
+                if(m[i][j] != 0 && i!=toprow){
                     multdivrows(m, false, i, m[i][j]);
                     addsubrows(m, false, i, toprow);
                 }
@@ -102,15 +100,19 @@ public class OBE {
             utils.printMatrix(m);
             toprow++;
         }
+    }
 
-        // for(int i = 0; i<m.length; i++){
-        //     if(m[i][i]!=1) multdivrows(m, false, i, m[i][i]);
-        // }
+    public static int getFirstNonZeroIdx(double[][] m) {
+        for(int i = m.length-1; i>=0; i--){
+            if(!isRowZero(m, i, 0, m[i].length)) return i;
+        }
+        return -1;
     }
-    public static void main(String[] args) {
-        double m[][] = {{1,3,-2,0,2,0,0},{2,6,-5,-2,4,-3,-1},{0,0,5,10,0,15,5},{2,6,0,8,4,18,6}};
-        toEchelon(m);
-        System.out.println();
-        utils.printMatrix(m);
-    }
+    // public static void main(String[] args) {
+        // double m[][] = {{1,3,-2,0,2,0,0},{2,6,-5,-2,4,-3,-1},{0,0,5,10,0,15,5},{2,6,0,8,4,18,6}};
+    //     double m[][] = {};
+    //     toEchelon(m);
+    //     System.out.println();
+    //     utils.printMatrix(m);
+    // }
 }
