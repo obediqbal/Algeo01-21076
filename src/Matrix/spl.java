7e9761845acb334ebab60e5f7d9c504d2f34df37
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-09-30 19:44:30 +0700

    parametric for gauss jordan (masih ada bug)

--- src/Matrix/spl.java
+++ src/Matrix/spl.java
@@ -272,27 +272,39 @@ public class spl {
 		}
 		return res;
 	}
+
+	public static String[] eliminasiGaussJordan(double[][] m, boolean parametric){
+		double[][] nm = new double[m[0].length][m[0].length];
+		utils.forceCopyMatrix(m, nm);
+		OBE.toEchelon(nm, true);
+		
+		double[] res = eliminasiGaussJordan(m);
+		return resListToParametric(res, m, nm, parametric);
+	}
 	// public static void main(String[] args) {
 	// 	int a = 3;
 	// 	int b = 4;
 	// 	// double[][] m = new double[a][b];
 	// 	// utils.readMatrix(m, a, b);
-	// 	// double[][] m = {{1, -1, 2, 5}, {2, -2, 4, 10}, {3, -1, 6, 15}}; //parametrik
+	// 	double[][] m = {{1, -1, 2, 5}, {2, -2, 4, 10}, {3, -1, 6, 15}}; //parametrik
 	// 	// double[][] m ={{2,3,-1,5},{-2,3,-1,1},{4,4,-3,3}}; // punya solusi
+	// 	// double[][] m ={{2,3,-1,5},{4,4,-3,3},{-2,3,-1,1}}; // punya solusi
     //     // double m[][] = {{1,3,-2,0,2,0,0},{2,6,-5,-2,4,-3,-1},{0,0,5,10,0,15,5},{2,6,0,8,4,18,6}}; // parametrik
 	// 	// double[][] m = {{1,2,1,1},{2,2,0,2},{3,4,1,2}}; //Tidak ada solusi
 	// 	// utils.printMatrix(m);
 		
 	// 	// double[][] m ={{1,1,-1,-1,1},{2,5,-7,-5,-2},{2,-1,1,3,4},{5,2,-4,2,6}};
-	// 	double[][] m = {{1,-1,0,0,1,3},{1,1,0,-3,0,6},{2,-1,0,1,-1,5},{-1,2,0,-2,-1,-1}};
+	// 	// double[][] m = {{1,-1,0,0,1,3},{1,1,0,-3,0,6},{2,-1,0,1,-1,5},{-1,2,0,-2,-1,-1}};
 	// 	// String[] res = eliminasiGaussJordan(m);
 	// 	// utils.printSolusi(res);;
-	// 	String[] res = eliminasiGauss(m, true);
-	// 	double[] dres = eliminasiGauss(m);
+	// 	String[] gres = eliminasiGauss(m, true);
+	// 	// double[] dres = eliminasiGauss(m);
+	// 	String[] gjres = eliminasiGaussJordan(m, true);
+	// 	// double[] dres = eliminasiGaussJordan(m);
 	// 	System.out.println();
-	// 	utils.printSolusi(res);
+	// 	utils.printSolusi(gres);
 	// 	System.out.println();
-	// 	utils.printSolusi(dres);
+	// 	utils.printSolusi(gjres);
 	// 	// double[][] res = eliminasiGauss(m);
 	// 	// res = eliminasiGauss(m);
 	// }
